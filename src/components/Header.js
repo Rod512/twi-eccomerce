5b8aec4236686f129ce0c0a7f68c9e63e16a21a5
Rod512 <rudro149187@bsdi-bd.org>
2023-03-25 00:11:01 +0600

    slidebar done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, {useContext} from 'react';
+// slidebar context
+import { SlidebarContext } from '../contexts/SidebarContext';
+// import icons
+import {BsBag} from 'react-icons/bs';
 
 const Header = () => {
+    const {isOpen,setIsOpen} = useContext(SlidebarContext);
     return (
-        <div>
-            header
-        </div>
+       <header className='bg-pink-200'>
+        <div>Header</div>
+        <div className='cursor-pointer flex relative' onClick={() => setIsOpen(!isOpen)}><BsBag className='text-2xl'></BsBag></div>
+       </header>
     );
 };
 
